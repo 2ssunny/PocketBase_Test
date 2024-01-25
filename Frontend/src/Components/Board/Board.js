68036c86d245582ac8a70c63019d53a1264311a7
Juho Lee <eungseoni@gmail.com>
2024-01-24 21:33:41 -0500

    feat: queing posts on the board page

--- Frontend/src/Components/Board/Board.js
+++ Frontend/src/Components/Board/Board.js
@@ -1,7 +1,37 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PocketBase from "pocketbase";
+
+import "./board.css";
 
 function App() {
+  const pb = new PocketBase("http://127.0.0.1:8090");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    const fetchData = async () => {
+      try {
+        const records = await pb.collection("Posts").getFullList({
+          sort: "-created",
+        });
+        if (!isCancelled) {
+          console.log(records); // API 응답 로그 출력
+          setPosts(records);
+        }
+      } catch (error) {
+        if (!isCancelled) {
+          console.error(error);
+        }
+      }
+    };
+    fetchData();
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
   return (
     <div>
       <header className="title-header">
@@ -13,6 +43,20 @@ function App() {
           <span className="title-text2">Upload</span>
         </Link>
       </header>
+      {posts.map((post, index) => {
+        return (
+          <div key={index} className="boardList">
+            <b className="text-muted">Title:</b>
+            <p>{post.Title}</p>
+            <b className="text-muted">Body:</b>
+            <p>{post.Body}</p>
+            <b className="text-muted">AttachedPhoto:</b>
+            <p>{post.Photo}</p>
+            <b className="text-muted">PostingDate:</b>
+            <p>{post.created}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
