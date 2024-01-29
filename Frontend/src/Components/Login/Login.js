ceff40b5fbbbbdcdefdc7a5852dbd5d1df03707d
Juho Lee <eungseoni@gmail.com>
2024-01-29 12:57:09 -0500

    feat: Login and determine whether login when post

---
+++ Frontend/src/Components/Login/Login.js
@@ -0,0 +1,66 @@
+import PocketBase from "pocketbase";
+import React, { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./login.css";
+
+function App() {
+  const pb = new PocketBase("http://127.0.0.1:8090/");
+
+  const navigate = useNavigate();
+
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async () => {
+    const data = {
+      email,
+      password,
+    };
+    pb.collection("users")
+      .authWithPassword(data.email, data.password)
+      .then((authData) => {
+        if (authData.record) {
+          // 로그인 성공, 사용자 이름 저장
+          localStorage.setItem("username", authData.record.username);
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        if (error.status === 400) {
+          // 로그인 실패, 오류 메시지 표시
+          alert("로그인 정보를 확인한 후 다시 시도해주세요.");
+        } else {
+          console.error(error);
+        }
+      });
+  };
+
+  return (
+    <div>
+      <label>
+        E-mail:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+
+      <br />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+  );
+}
+
+export default App;
--- Frontend/src/Components/home.js
+++ Frontend/src/Components/home.js
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import PocketBase from "pocketbase";
 
 function App() {
+  const pb = new PocketBase("http://127.0.0.1:8090/");
+  const username = localStorage.getItem("username");
+
+  const handleLogout = () => {
+    pb.authStore.clear();
+    localStorage.removeItem("username");
+    window.location.reload();
+  };
+
   return (
     <div>
       <header className="title-header">
+        {username ? <p>{username}님 환영합니다</p> : <p>로그인을 해주세요</p>}
         <Link to="/" className="title-text">
           <span className="title-text2">Home</span>
         </Link>
@@ -21,6 +32,7 @@ function App() {
           <span className="title-text2">Board</span>
         </Link>
       </header>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
